5c758bbe48510f2afe528581162ae57c564f0a67
DexterFF <mohamedhelalilyes@gmail.com>
2020-10-24 15:58:12 +0200

    Phase I : Feature 2 : Elevator Lift

---
+++ Assets/Scripts/CheckForPlayerInElevator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForPlayerInElevator : MonoBehaviour
+{
+    private Transform _elevator;
+
+    private void Start()
+    {
+        _elevator = transform.parent;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.parent = _elevator;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _levels = new List<Transform>();
+    private Transform _destination;
+    [SerializeField]
+    private float _elevatorSpeed;
+    [SerializeField]
+    private float _pauseDuration;
+    private WaitForSeconds _delay;
+
+    private bool _inverse;
+    private int _levelIndex = 1;
+
+    private bool _pauseElevator;
+
+    private void Start()
+    {
+        _delay = new WaitForSeconds(_pauseDuration);
+        if(_levels != null)
+            transform.position = _levels[0].position;
+
+        _destination = _levels[1];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(_pauseElevator == false)
+            ElevatorMouvements();
+    }
+
+    void ElevatorMouvements()
+    {
+        if (transform.position == _destination.position)
+        {
+            if (_inverse == false)
+                _levelIndex++;
+            else
+                _levelIndex--;
+
+            if (_levelIndex == _levels.Count)
+            {
+                _inverse = true;
+                _levelIndex -= 2;
+            }
+            else if (_levelIndex < 0)
+            {
+                _inverse = false;
+                _levelIndex += 2;
+            }
+
+            _destination = _levels[_levelIndex];
+            StartCoroutine(PauseDelay());
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _destination.position, Time.deltaTime * _elevatorSpeed);
+    }
+
+    IEnumerator PauseDelay()
+    {
+        _pauseElevator = true;
+        yield return _delay;
+        _pauseElevator = false;
+    }
+}
